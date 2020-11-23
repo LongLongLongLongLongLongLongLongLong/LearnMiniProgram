c3dff357cca18503e7bc4fdb71a40e18e20407f2
Gao shenggang <gao.shenggang@h3c.com>
2020-11-23 10:29:35 +0800

    注册App可以做什么事

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,14 @@
 // pages/home/home.js
+
+const app = getApp(); //获取示例对象
+
 Page({
+  handleGetUserInfo(event){
+    console.log(event);
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    // 以下两行为演示全局变量使用
+    console.log(app.globalData.userName);
+    console.log(app.globalData.age);
 
   }
 })
