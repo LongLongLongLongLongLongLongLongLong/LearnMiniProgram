ca091f86e3e0b9889a7c283a022e2701f091b8a0
Gao shenggang <gao.shenggang@h3c.com>
2020-11-24 19:17:12 +0800

    page生命周期

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,12 +3,36 @@
 const app = getApp(); //获取示例对象
 
 Page({
+
+  data(){
+    message:"哈哈";
+  },
   handleGetUserInfo(event){
     console.log(event);
 
     // 以下两行为演示全局变量使用
     console.log(app.globalData.userName);
     console.log(app.globalData.age);
+  },
 
-  }
+
+  onLoad(){
+    console.log("onLoad");
+  },
+
+  onReady(){
+    console.log("onReady");
+  },
+
+  onShow(){
+    console.log("onShow");
+  },
+  onHide(){
+    console.log("onHide");
+  },
+
+  onUnload(){
+    console.log("onUnload");
+  },
+
 })
