8d458f7e9dd3b447c4a2171803ad98fbf81647c1
Gao shenggang <gao.shenggang@h3c.com>
2020-12-09 14:29:13 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,32 @@
-// pages/home/home.js
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    console.log('click');
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  touchstart(){
+    console.log('touchstart');
+  },
+  touchmove(){
+    console.log('touchmove');
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  touchend(){
+    console.log('touchend');
+  },
+  tap(){
+    console.log('tap');
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  longpress(){
+    console.log('longpress');
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ 
 })
