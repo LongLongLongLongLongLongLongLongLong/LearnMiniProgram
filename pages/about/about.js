fde9379d4e40ed843a4178337369ee542ef3278b
Gao shenggang <gao.shenggang@h3c.com>
2020-12-14 17:08:13 +0800

    网络封装

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,4 +1,6 @@
 // pages/about/about.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,25 +14,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    request({
+      url:'http://123.207.32.32:8000/recommend'
+    }).then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+
+    })
+
       //1.发送网络请求
-      // wx.request({
-      //   url: 'http://123.207.32.32:8000/recommend',
-      //   success:function(res){
-      //     console.log(res);
-      //   }
-      // })
+      wx.request({
+        url: 'http://123.207.32.32:8000/recommend',
+        success:function(res){
+          console.log(res);
+        }
+      })
 
       //2.发送get请求并携带参数
-      // wx.request({
-      //   url: 'http://123.207.32.32:8000/recommend',
-      //   data:{
-      //     type:'sell',
-      //     page:1
-      //   },
-      //   success:function(res){
-      //       console.log;(res)
-      //   }
-      // })
+      wx.request({
+        url: 'http://123.207.32.32:8000/recommend',
+        data:{
+          type:'sell',
+          page:1
+        },
+        success:function(res){
+            console.log;(res)
+        }
+      })
 
       //3.发送post请求并携带参数
       wx.request({
